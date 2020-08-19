c2f819c814e68a7a6c7859d7489827c6bd887f97
EmefaLinda <lindaemf@gmail.com>
2020-08-19 16:31:02 -0700

    Updates

---
+++ precache-manifest.f270dd3f3c03449ca595f69446eca781.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e80319a2a903e0a9d3284facd3162a5e",
+    "url": "/HW3_99RW9JFRMO/index.html"
+  },
+  {
+    "revision": "95a5ba08ba7413bc1143",
+    "url": "/HW3_99RW9JFRMO/static/css/main.1b734b52.chunk.css"
+  },
+  {
+    "revision": "9e258eb5189d23d888f0",
+    "url": "/HW3_99RW9JFRMO/static/js/2.c1ee4938.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/HW3_99RW9JFRMO/static/js/2.c1ee4938.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95a5ba08ba7413bc1143",
+    "url": "/HW3_99RW9JFRMO/static/js/main.f788b7d6.chunk.js"
+  },
+  {
+    "revision": "e1b4fb61fc6158047c88",
+    "url": "/HW3_99RW9JFRMO/static/js/runtime-main.cf169a72.js"
+  },
+  {
+    "revision": "5f1cd582c6d4fb5f4d86e6c1635deec2",
+    "url": "/HW3_99RW9JFRMO/static/media/hulk-1.5f1cd582.jpg"
+  },
+  {
+    "revision": "294dd39b7ab19aedb004eb8f359c307f",
+    "url": "/HW3_99RW9JFRMO/static/media/venom-1.294dd39b.jpg"
+  },
+  {
+    "revision": "5eb62fe8d11e8a32b2fb2554088f308e",
+    "url": "/HW3_99RW9JFRMO/static/media/venom-2.5eb62fe8.jpg"
+  }
+]);
